fe65fee01ab5d8989ced6fb8bac033362d0186cd
Judy Chuang <tw26581129@gamil.com>
2022-02-01 23:46:06 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./index.css";
+import styled from "styled-components";
+
+import Memegenerator from "./components/Memegenerator";
+
+function App() {
+  return (
+    <Wrapper>
+      <div class="title">
+        <h1>MEME GENERATOR</h1>
+      </div>
+      <Memegenerator />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  .title {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    left: 0;
+    height: 5rem;
+    background-color: #000;
+    text-align: center;
+  }
+  .title h1 {
+    color: #fff;
+  }
+`;
+export default App;
---
+++ src/components/Memegenerator.js
@@ -0,0 +1,148 @@
+import React, { useState,useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+const Memegenerator = () => {
+  const [inputText, setInputText] = useState({
+    topText:"",
+    bottomText:"",
+  });
+  
+  const [allImage, setAllImage] = useState([]);
+  const [randomImage,setRandomImage]=useState("https://i.imgflip.com/26am.jpg");
+
+//function to fetch data
+  const fetchImage = async (url) => {
+    try {
+      const res = await axios.get(url);
+      const images = res.data;
+      setAllImage(images.data.memes);
+      console.log(allImage);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
+//function to change the value of input fields
+  const handleChange=(e)=>{
+    setInputText({...inputText,[e.target.name]:e.target.value,})
+  }
+    //function to submit from and create meme
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    const random=allImage[Math.floor(Math.random()*allImage.length)].url;
+    setRandomImage(random);
+  }
+useEffect(()=>{
+  fetchImage("https://api.imgflip.com/get_memes");
+},[]);
+
+  return (
+    <Wrapper>
+      <form className="input-section" onSubmit={submitHandler}>
+          <input type="text" name="topText" placeholder="Enter the top text" value={inputText.topText} onChange={handleChange}/>
+          <input type="text" name="bottomText" placeholder="Enter the bottom text" value={inputText.bottomText} onChange={handleChange} />
+          <button >
+            Generate
+          </button> 
+      </form>
+      <div className="image-section">
+        <img className="image" src={randomImage} alt="meme"/>
+        <h2 className="top-text text">{inputText.topText}</h2>
+        <h2 className="bottom-text text">{inputText.bottomText}</h2>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  .input-section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1.2rem;
+    height: 10rem;
+    margin-top:3rem;
+  }
+  input,
+  button {
+    font-family: inherit;
+    font-size: 2rem;
+    padding: 0.5rem 1rem;
+    border-radius: 10px;
+    border: 1px solid #000;
+  }
+  button {
+    cursor: pointer;
+    background-color: #000;
+    color: #fff;
+    transition: all linear 0.3s;
+  }
+  button:hover {
+    background-color: #fff;
+    color: #000;
+  }
+  .image-section {
+    height: 25rem;
+    width: 50rem;
+    position:relative;
+  }
+  .image{
+    height:25rem;
+    width:40rem;
+    }
+  
+  .text{
+    color:#fff;   
+    text-align:center;
+    font-family:"impact",sans-serif;
+    font-weight:lighter;
+    letter-spacing:2px;
+    font-size:3rem;
+    text-transform:uppercase;
+    text-shadow:1px 1px 5px #000;
+    left:50%;
+    transform: translateX(-50%);
+    width:100%;
+  }
+  .top-text{
+    position:absolute;
+    top:0;
+  }
+  .bottom-text{
+    position:absolute;
+    bottom:0;
+  }
+  @media only screen and (max-width:992px){
+    input,
+    button{
+      font-family:1rem;
+      padding:0.2rem 0.4rem;
+    }
+    .input-section{
+      flex-direction:column;
+      margin-top:12rem;
+    }
+    .image-section{
+      height:20rem;
+      width:20rem;
+      margin-bottom:2rem;
+    }
+    .image{
+    height:20rem;
+    width:20rem;
+    
+    }
+  }
+
+
+`;
+
+export default Memegenerator;

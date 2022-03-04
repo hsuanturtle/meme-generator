4cfb5119d903e0836e8bd66e60a0fb55e65024e0
Judy Chuang <tw26581129@gamil.com>
2022-03-04 14:08:30 +0800

    add comments

--- src/components/useDraggable.js
+++ src/components/useDraggable.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
 
 export default function useDraggable(e) {
+  // the distance that the text div is out of line
   const [offset, setOffset] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
+    //calculate the start point of the text
     const handleMouseDown = (e) => {
       const startX = e.clientX - offset.x;
       const startY = e.clientY - offset.y;
-      console.log(e.clientX, e.clientY, offset.x, offset.y);
 
+      //calculate how far the text has moved
       const handleMouseMove = (e) => {
         const newX = e.pageX - startX;
         const newY = e.pageY - startY;
@@ -26,11 +28,13 @@ export default function useDraggable(e) {
     };
 
     e.current.addEventListener("mousedown", handleMouseDown);
+
     return () => {
       e.current.removeEventListener("mousedown", handleMouseDown);
     };
   }, [offset.x, offset.y]);
 
+  //show the text is moving
   useEffect(() => {
     e.current.style.transform = `translate(${offset.x}px,${offset.y}px)`;
   }, [offset.x, offset.y]);
